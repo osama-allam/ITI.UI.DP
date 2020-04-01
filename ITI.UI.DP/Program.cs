ca6f833000204c29b228e62d2cd7345858809a6d
Osama Allam <osama.allam@tpay.me>
2020-04-01 07:15:19 +0200

    Add project files.

---
+++ ITI.UI.DP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITI.UI.DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
